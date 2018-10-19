35dc8d090d2100e2dab5a2167af059f04b8f3d9b
Salvatore Architetto <sal.architetto862@gmail.com>
2018-10-18 22:19:34 -0400

    created database + table for initial start. Also added a Italy footy csv.

--- scrape_data/create_db.py
+++ scrape_data/create_db.py
@@ -7,3 +7,73 @@ and create either a mysql db or sqlite db to
 store the data into.
 
 """
+
+import sqlite3
+from sqlite3 import Error
+
+#Creating DB to specific directory
+
+#code to create table
+
+def create_connection(db_file):
+    """"
+    Attemptin to create the DB in SQLite
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        print("Connection Made.")
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+def create_table(conn, create_table_footy):
+
+    try:
+        c = conn.cursor()
+        c.execute(create_table_footy)
+    except Error as e:
+        print(e)
+
+def main():
+    path = "insert path name here"
+
+    footy = """
+          CREATE TABLE IF NOT EXISTS matches (
+              id integer PRIMARY KEY,
+              home_team text NOT NULL,
+              away_team text NOT NULL,
+              home_team_goals integer NOT NULL,
+              away_team_goals integer NOT NULL,
+              full_time_results text NOT NULL,
+              ht_home_goals integer NOT NULL,
+              ht_away_goals integer NOT NULL,
+              ht_result text NOT NULL,
+              attendance integer NULL,
+              home_team_shots integer,
+              away_team_shots integer,
+              home_team_shot_tar integer,
+              away_team_shot_tar integer,
+              home_woodwork integer,
+              away_woodwork integer, 
+              home_corner integer, 
+              away_corner integer, 
+              home_foul integer,
+              away_foul integer, 
+              home_yellow integer, 
+              away_yellow integer,
+              home_red integer, 
+              away_red integer
+          );
+      """
+    conn = create_connection(path)
+
+    if conn is not None:
+
+        create_table(conn, footy)
+    else:
+        print("Looks like theres something wrong with the connection!")
+
+if __name__ == '__main__':
+   main()
