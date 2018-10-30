49db81537ff675765de67cc19786ce3e4202b96d
Salvatore Architetto <sal.architetto862@gmail.com>
2018-10-29 22:59:22 -0400

    Adding file to clean csv data as there are empty values scattered throughout.

---
+++ scrape_data/clean_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import glob
+import os
+
+def read_dir(directory):
+    """
+    loop through a directory to get a specific
+    csv path
+    :param directory: initial strarting directory
+    :return: list of csv directories
+    """
+    files = os.listdir(directory)
+    file_path = []
+
+    for x in files:
+        file_path.append(directory + "\\" + x)
+
+    return file_path
+
+def remove_null_values(csv):
+    """
+    Function to remove all '' (empty data) from csv
+    :param csv: directory pointer to csv
+    :return: rewritten data to file
+    """
+
+    for x in csv:
+        print(x)
+        read_csv = pd.read_csv(x)
+        print(read_csv.head(5))
+        modified_csv = read_csv.fillna(0)
+
+        check = modified_csv.isnull().sum().sum()
+        print(check)
+
+        if check > 0:
+            print("Error: still '' values")
+        else:
+            print("There is no '' values")
+
+        #resaving to csv
+        modified_csv.to_csv(x)
